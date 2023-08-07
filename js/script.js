dfefed26160ec2df23f6da12a2281803503b967d
daida <138858179+daidasan@users.noreply.github.com>
2023-08-07 22:37:02 +0900

    モーダル

--- js/script.js
+++ js/script.js
@@ -5,41 +5,41 @@
 
 var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
 if (windowwidth > 768) {
-	var responsiveImage = [ //PC用の画像
-		{
-			src: './img/bigbg01.jpg'
-		},
-		{
-			src: './img/bigbg02.jpg'
-		},
-		// {
-		// 	src: './img/bigbg03.jpg'
-		// }
-	];
+    var responsiveImage = [ //PC用の画像
+        {
+            src: './img/bigbg01.jpg'
+        },
+        {
+            src: './img/bigbg02.jpg'
+        },
+        // {
+        // 	src: './img/bigbg03.jpg'
+        // }
+    ];
 } else {
-	var responsiveImage = [ //タブレットサイズ（768px）以下用の画像
-		{
-			src: './img/bigbg01.jpg'
-		},
-		{
-			src: './img/sp-bigbg02.jpg'
-		},
-		// {
-		// 	src: './img/bigbg03.jpg'
-		// }
-	];
+    var responsiveImage = [ //タブレットサイズ（768px）以下用の画像
+        {
+            src: './img/bigbg01.jpg'
+        },
+        {
+            src: './img/sp-bigbg02.jpg'
+        },
+        // {
+        // 	src: './img/bigbg03.jpg'
+        // }
+    ];
 }
 
 //Vegas全体の設定
 
 $('#slider').vegas({
-	transition: 'blur', //切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
-	transitionDuration: 2000, //切り替わりのアニメーション時間をミリ秒単位で設定
-	delay: 10000, //スライド間の遅延をミリ秒単位で。
-	animationDuration: 20000, //スライドアニメーション時間をミリ秒単位で設定
-	animation: 'kenburnsUp', //スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
-	slides: responsiveImage, //画像設定を読む
-	//timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
+    transition: 'blur', //切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
+    transitionDuration: 2000, //切り替わりのアニメーション時間をミリ秒単位で設定
+    delay: 10000, //スライド間の遅延をミリ秒単位で。
+    animationDuration: 20000, //スライドアニメーション時間をミリ秒単位で設定
+    animation: 'kenburnsUp', //スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
+    slides: responsiveImage, //画像設定を読む
+    //timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
 });
 
 
@@ -50,13 +50,13 @@ $('#slider').vegas({
 //gnav----------------------------------------------
 
 $(".openbtn").click(function () { //ボタンがクリックされたら
-	$(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
-	$("#g-nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
+    $(this).toggleClass('active'); //ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
 });
 
 $("#g-nav a").click(function () { //ナビゲーションのリンクがクリックされたら
-	$(".openbtn").removeClass('active'); //ボタンの activeクラスを除去し
-	$("#g-nav").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスも除去
+    $(".openbtn").removeClass('active'); //ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスも除去
 });
 
 
@@ -88,24 +88,24 @@ class Modal {
         event.stopPropagation();
         event.preventDefault();
 
-    // href要素を取得
-    const targetHash = this.DOM.targetELm.href;
-     
-    // テキストの場合
-      if(targetHash.indexOf('#') > 0) {
+        // href要素を取得
+        const targetHash = this.DOM.targetELm.href;
+
+        // テキストの場合
+        if (targetHash.indexOf('#') > 0) {
             const targetSelector = targetHash.replace(/.*(#.*)/, '$1');
             // モーダルする要素を取得
             const targetHTML = document.querySelector(targetSelector).innerHTML;
             const elmDIV = document.createElement('div');
-                  elmDIV.innerHTML = targetHTML;
+            elmDIV.innerHTML = targetHTML;
             this.DOM.modalElm = elmDIV;
         }
         // 画像の場合
         console.log(targetHash.indexOf('jpg') > 0);
-        if(targetHash.indexOf('jpg') > 0 || targetHash.indexOf('jpeg') > 0 || targetHash.indexOf('png') > 0 || targetHash.indexOf('gif') > 0){
+        if (targetHash.indexOf('jpg') > 0 || targetHash.indexOf('jpeg') > 0 || targetHash.indexOf('png') > 0 || targetHash.indexOf('gif') > 0) {
             const imgElm = document.createElement('img');
-                  imgElm.src = targetHash;
-                  imgElm.id = "targetElm";
+            imgElm.src = targetHash;
+            imgElm.id = "targetElm";
 
             this.DOM.modalElm = imgElm;
         }
@@ -132,8 +132,8 @@ class Modal {
     // モーダル表示
     _modalShow() {
         const targetmodalItem = document.createElement('div');
-              targetmodalItem.appendChild(this.DOM.modalElm);
-        
+        targetmodalItem.appendChild(this.DOM.modalElm);
+
         document.body.appendChild(targetmodalItem);
         targetmodalItem.classList.add('modal', 'visible');
         targetmodalItem.classList.add('fadein');
@@ -159,7 +159,7 @@ class Modal {
     // モーダル非表示
     _disappearModal() {
         // this.DOM.modalElm.classList.remove('modal', 'visible');
-        document.querySelector(".modal-overlay").remove();
+        document.querySelector("#information").remove();
         document.querySelector(".modal-closebtn").remove();
         document.querySelector(".modal").remove();
         document.body.style.overflow = "visible";
@@ -171,5 +171,3 @@ const targetBtns = document.querySelectorAll(".modalBtn");
 targetBtns.forEach((targetBtn) => {
     const modal = new Modal(targetBtn);
 });
-
-
