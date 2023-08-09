7753b6d9497ac8d4ea7dcdb40ab66f782ebd0cb4
daida <138858179+daidasan@users.noreply.github.com>
2023-08-09 19:19:40 +0900

    works

---
+++ works/js/works.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+//定義する順番に注意
+// サムネイル
+const swiperThumbnail = new Swiper(".swiper-thumbnail", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  watchSlidesProgress: true,
+  // breakpoints: {
+  //   678: {
+  //     direction: "vertical"
+  //   }
+  // }
+});
+
+// スライダー
+const slider = new Swiper(".slider", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  autoplay: true,
+  loop: true,
+  thumbs: {
+    swiper: swiperThumbnail
+  }
+});
